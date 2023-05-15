086c721e80a7ee3e66a2d1c7328afb225d7bf515
Matthijs Meire <matthijs.meire@ugent.be>
2023-05-15 21:00:33 +0200

    Create StreamlitApp_ComplainerType.py

---
+++ StreamlitApp_ComplainerType.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+
+from transformers import pipeline
+
+
+from transformers import pipeline
+classifier = pipeline(model="matmei/complainerType")
+
+# Define your function f() here
+def f(text):
+    return classifier(text)
+
+def main():
+    # Set page title
+    st.title("Complainer classification App")
+
+    # Get user input text
+    user_input = st.text_input("Enter the complaint you want to analyze:")
+
+    # Apply function f() when Submit button is clicked
+    if st.button("Submit"):
+        result = f(user_input)
+        st.success("Classification: {}".format(result))
+
+if __name__ == "__main__":
+    main()
